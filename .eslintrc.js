7d56d68dc972040b48898de4a55f244c9571f2ab
a.masalov <delsol@bk.ru>
2020-02-14 01:13:04 +0300

    pretty

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended', 'standard', 'prettier', 'prettier/react'],
+}

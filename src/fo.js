589c01cbd63edd2b074e7609c3d2541108456e72
a.masalov <delsol@bk.ru>
2020-02-14 01:56:41 +0300

    spretty fo

--- src/fo.js
+++ src/fo.js
@@ -3,14 +3,17 @@ const superagent = require('superagent')
 const fo = function(generatorFunction) {
   const generator = generatorFunction()
 
-  next()
+  // Kick off the generator's execution
+  step()
 
   // Call next() or throw() on the generator as necessary
-  function next(v, isError) {
-    const res = isError ? generator.throw(v) : generator.next(v)
+  function step(value, isError) {
+    const res = isError ? generator.throw(value) : generator.next(value)
+
     if (res.done) {
       return
     }
+
     handleAsync(res.value)
   }
 
@@ -21,25 +24,52 @@ const fo = function(generatorFunction) {
     } else if (typeof async === 'function') {
       handleThunk(async)
     } else {
-      next(new Error(`Invalid yield ${async}`), true)
+      step(new Error(`Invalid yield ${async}`), true)
     }
   }
 
   // If the generator yielded a promise, call `.then()`
   function handlePromise(async) {
-    async.then(next, (error) => next(error, true))
+    async.then(step, (error) => step(error, true))
   }
 
   // If the generator yielded a thunk, call it
   function handleThunk(async) {
-    async((error, v) => {
-      error ? next(error, true) : next(v)
+    async((error, value) => {
+      error ? step(error, true) : step(value)
     })
   }
 }
 
-// fo in action
 fo(function*() {
-  const html = (yield superagent.get('http://www.google.com')).text
-  console.log('html', html)
+  try {
+    const response = yield superagent.get('error here')
+    console.log('response', response)
+  } catch (error) {
+    console.log(error.message)
+  }
+
+  const response = yield superagent.get('http://www.google.com')
+  const html = response.text
+  console.log('html', typeof html)
+})
+
+fo(function*() {
+  const url = 'http://doesnot.exist.baddomain'
+  const NUM_RETRIES = 3
+  let res
+
+  for (let i = 0; i < NUM_RETRIES; ++i) {
+    try {
+      // Going to yield 3 times, and `fo()` will call `generator.throw()`
+      // 3 times because superagent will fail every time
+      res = yield superagent.get(url)
+      break
+    } catch (error) {
+      console.log('retry')
+      /* retry */
+    }
+  }
+
+  console.log('res retry', res)
 })
